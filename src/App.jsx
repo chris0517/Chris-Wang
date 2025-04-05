4cccdc49662e48cfb9c6be73b6f6ec9021f606b7
Chris Wang <chris92563@outlook.com>
2025-04-04 22:12:32 -0400

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
-import Nav from './components/Nav';
 import Home from './components/Home';
 
 function App() {
